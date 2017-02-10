b3a6d94b71454cebeded511bf56a10d74fdfac5e
Josh Curl <josh@curl.me>
2017-02-10 13:12:46 -0800

    Remove most project actions

--- project/interface.go
+++ project/interface.go
@@ -16,22 +16,8 @@ type APIProject interface {
 	Build(ctx context.Context, options options.Build, sevice ...string) error
 	Config() (string, error)
 	Create(ctx context.Context, options options.Create, services ...string) error
-	Delete(ctx context.Context, options options.Delete, services ...string) error
-	Down(ctx context.Context, options options.Down, services ...string) error
 	Events(ctx context.Context, services ...string) (chan events.ContainerEvent, error)
-	Kill(ctx context.Context, signal string, services ...string) error
 	Log(ctx context.Context, follow bool, services ...string) error
-	Pause(ctx context.Context, services ...string) error
-	Ps(ctx context.Context, services ...string) (InfoSet, error)
-	// FIXME(vdemeester) we could use nat.Port instead ?
-	Port(ctx context.Context, index int, protocol, serviceName, privatePort string) (string, error)
-	Pull(ctx context.Context, services ...string) error
-	Restart(ctx context.Context, timeout int, services ...string) error
-	Run(ctx context.Context, serviceName string, commandParts []string, options options.Run) (int, error)
-	Scale(ctx context.Context, timeout int, servicesScale map[string]int) error
-	Start(ctx context.Context, services ...string) error
-	Stop(ctx context.Context, timeout int, services ...string) error
-	Unpause(ctx context.Context, services ...string) error
 	Up(ctx context.Context, options options.Up, services ...string) error
 
 	Parse() error
