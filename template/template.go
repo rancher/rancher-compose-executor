5d1e17aa1be766623114ba05d35299d926e29445
Josh Curl <josh@curl.me>
2017-03-16 17:14:36 -0700

    Add the ability to skip templating

--- template/template.go
+++ template/template.go
@@ -2,12 +2,19 @@ package template
 
 import (
 	"bytes"
+	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
 )
 
 func Apply(contents []byte, variables map[string]string) ([]byte, error) {
+	// Skip templating if contents begin with '# notemplating'
+	trimmedContents := strings.TrimSpace(string(contents))
+	if strings.HasPrefix(trimmedContents, "#notemplating") || strings.HasPrefix(trimmedContents, "# notemplating") {
+		return contents, nil
+	}
+
 	t, err := template.New("template").Funcs(sprig.TxtFuncMap()).Parse(string(contents))
 	if err != nil {
 		return nil, err
