8f961f5dc7c23eb78542785e8ac12db3daf4930d
Josh Curl <josh@curl.me>
2017-07-07 17:11:14 -0700

    Add splitPreserveQuotes function to templating

--- template/template.go
+++ template/template.go
@@ -6,6 +6,7 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig"
+	"github.com/rancher/rancher-compose-executor/template/funcs"
 )
 
 type ReleaseInfo struct {
@@ -20,7 +21,10 @@ func Apply(contents []byte, releaseInfo ReleaseInfo, variables map[string]string
 		return contents, nil
 	}
 
-	t, err := template.New("template").Funcs(sprig.TxtFuncMap()).Parse(string(contents))
+	templateFuncs := sprig.TxtFuncMap()
+	templateFuncs["splitPreserveQuotes"] = funcs.SplitPreserveQuotes
+
+	t, err := template.New("template").Funcs(templateFuncs).Parse(string(contents))
 	if err != nil {
 		return nil, err
 	}
