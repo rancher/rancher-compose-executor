5b9d7eb59a794d9f3cee7d17cd802284ead80822
Josh Curl <josh@curl.me>
2017-09-20 09:25:32 -0700

    Fix selector and legacy load balancers

--- resources/service_order_test.go
+++ resources/service_order_test.go
@@ -4,41 +4,70 @@ import (
 	"testing"
 
 	"github.com/rancher/rancher-compose-executor/config"
+	"github.com/rancher/rancher-compose-executor/convert"
 )
 
 func TestGetServiceOrder(t *testing.T) {
 	testGetServiceOrder(t, map[string]*config.ServiceConfig{
-		"s1": &config.ServiceConfig{},
-		"s2": &config.ServiceConfig{},
+		"s1": {},
+		"s2": {},
 	}, []string{"s1", "s2"})
 
-	testGetServiceOrder(t, map[string]*config.ServiceConfig{
-		"s1": &config.ServiceConfig{},
-		"s2": &config.ServiceConfig{},
-		"lb": lbConfigFactory("s1", "s2"),
-	}, []string{"s1", "s2"}, []string{"lb"})
+	for _, factory := range []func(...string) *config.ServiceConfig{
+		lbConfigFactory,
+		legacyLbConfigFactory,
+	} {
+		testGetServiceOrder(t, map[string]*config.ServiceConfig{
+			"s1": {},
+			"s2": {},
+			"lb": factory("s1", "s2"),
+		}, []string{"s1", "s2"}, []string{"lb"})
 
-	testGetServiceOrder(t, map[string]*config.ServiceConfig{
-		"s1":  &config.ServiceConfig{},
-		"s2":  &config.ServiceConfig{},
-		"lb":  lbConfigFactory("s1", "s2"),
-		"lb2": lbConfigFactory("lb"),
-	}, []string{"s1", "s2"}, []string{"lb"}, []string{"lb2"})
+		testGetServiceOrder(t, map[string]*config.ServiceConfig{
+			"s1":  {},
+			"s2":  {},
+			"lb":  factory("s1", "s2"),
+			"lb2": factory("lb"),
+		}, []string{"s1", "s2"}, []string{"lb"}, []string{"lb2"})
 
+		testGetServiceOrder(t, map[string]*config.ServiceConfig{
+			"s1":  {},
+			"s2":  {},
+			"lb":  factory("s1", "s2"),
+			"lb2": factory("lb"),
+		}, []string{"s1", "s2"}, []string{"lb"}, []string{"lb2"})
+
+		testGetServiceOrder(t, map[string]*config.ServiceConfig{
+			"s1":  {},
+			"s2":  {},
+			"lb":  factory("s1", "s2"),
+			"lb2": factory("lb"),
+			"lb3": factory("lb"),
+			"lb4": factory("lb2", "lb3"),
+		}, []string{"s1", "s2"}, []string{"lb"}, []string{"lb2", "lb3"}, []string{"lb4"})
+	}
+}
+
+func TestGetServiceOrderWithSelector(t *testing.T) {
 	testGetServiceOrder(t, map[string]*config.ServiceConfig{
-		"s1":  &config.ServiceConfig{},
-		"s2":  &config.ServiceConfig{},
-		"lb":  lbConfigFactory("s1", "s2"),
-		"lb2": lbConfigFactory("lb"),
-		"lb3": lbConfigFactory("lb"),
-		"lb4": lbConfigFactory("lb2", "lb3"),
-	}, []string{"s1", "s2"}, []string{"lb"}, []string{"lb2", "lb3"}, []string{"lb4"})
+		"lb": {
+			RancherConfig: config.RancherConfig{
+				LbConfig: &config.LBConfig{
+					PortRules: []config.PortRule{
+						{
+							Selector: "foo=bar",
+						},
+					},
+				},
+			},
+		},
+	}, []string{"lb"})
 }
 
 func TestGetServiceOrderCycleFails(t *testing.T) {
 	_, err := getServiceOrder(nil, map[string]*config.ServiceConfig{
-		"s1":  &config.ServiceConfig{},
-		"s2":  &config.ServiceConfig{},
+		"s1":  {},
+		"s2":  {},
 		"lb":  lbConfigFactory("s1", "s2"),
 		"lb2": lbConfigFactory("lb3"),
 		"lb3": lbConfigFactory("lb2"),
@@ -61,6 +90,13 @@ func lbConfigFactory(targetServices ...string) *config.ServiceConfig {
 				PortRules: portRules,
 			},
 		},
+	}
+}
+
+func legacyLbConfigFactory(links ...string) *config.ServiceConfig {
+	return &config.ServiceConfig{
+		Image: convert.LegacyLBImage,
+		Links: links,
 	}
 }
 
